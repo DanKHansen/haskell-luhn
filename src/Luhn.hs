e0699ae7a172d5e27ba1c04676e80324e887d239
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-15 13:30:26 +0200

    solution

--- src/Luhn.hs
+++ src/Luhn.hs
@@ -1,4 +1,13 @@
 module Luhn (isValid) where
 
+import Data.Char (digitToInt, isSpace)
+
 isValid :: String -> Bool
-isValid n = error "You need to implement this function."
+isValid n = (sum norm `mod` 10 == 0) && (length noSpace > 1)
+  where
+    noSpace = filter (not . isSpace) n
+    norm =
+      [ let v = digitToInt d * (1 + (i `mod` 2))
+         in (v + if v > 9 then -9 else 0) `mod` 10
+        | (d, i) <- zip (reverse noSpace) [0 ..]
+      ]
